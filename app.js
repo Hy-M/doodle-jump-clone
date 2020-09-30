e66af6228b16f3295b0c638acd8e5d4c5dac204f
Hy-M <humayraa.m@outlook.com>
2020-09-30 12:15:06 +0100

    create createDoodler()

---
+++ app.js
@@ -0,0 +1,6 @@
+const grid = document.querySelector(".grid");
+const doodler = document.createElement("div");
+let doodlerLeftSpace = 50;
+let doodlerBottomSpace = 150;
+let isGameOver = false;
+let platCount = 5;
